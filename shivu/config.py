46cbe2d34467ca3ac57f0458c71f6d208be0fd32
dragxdd7 <2313181058316@thenewcollege.edu.in>
2025-01-31 01:11:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "7172020159"
+    sudo_users = "7361622601"
+    GROUP_ID = -1002382022736
+    TOKEN = "7527408650:AAEB-iYs6N9kAbnkSukqmXU28LEQubTOgOk"
+    mongo_url = "mongodb+srv://xdragxt:drag123@dragtest.2b4zj.mongodb.net/?retryWrites=true&w=majority&appName=dragtest"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    CHARA_CHANNEL_ID = "-1002393685933"
+    api_id = 7155513
+    api_hash = "aea00e5d96c3eb15647ca25c0f93a234"
 
     
 class Production(Config):
